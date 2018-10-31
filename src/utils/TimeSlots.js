4a5424572a419750114735666b6b45fe0f6f841a
mannynotfound <manuel.palou@buzzfeed.com>
2018-10-31 16:14:14 -0400

    add dst fix

--- src/utils/TimeSlots.js
+++ src/utils/TimeSlots.js
@@ -1,7 +1,7 @@
 import dates from './dates'
 
 const getDstOffset = (start, end) =>
-  Math.abs(start.getTimezoneOffset() - end.getTimezoneOffset())
+  start.getTimezoneOffset() - end.getTimezoneOffset()
 
 const getKey = (min, max, step, slots) =>
   `${+dates.startOf(min, 'minutes')}` +
@@ -107,11 +107,11 @@ export function getSlotMetrics({ min: start, max: end, step, timeslots }) {
 
       const rangeStartMin = positionFromDate(rangeStart)
       const rangeEndMin = positionFromDate(rangeEnd)
-      const top = rangeStartMin / totalMin * 100
+      const top = (rangeStartMin / totalMin) * 100
 
       return {
         top,
-        height: rangeEndMin / totalMin * 100 - top,
+        height: (rangeEndMin / totalMin) * 100 - top,
         start: positionFromDate(rangeStart),
         startDate: rangeStart,
         end: positionFromDate(rangeEnd),
